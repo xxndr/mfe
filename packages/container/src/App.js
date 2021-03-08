da0e309a02039f0c4b449f3c4e3f551ec0506269
ksandr68 <malinsancho@gmail.com>
2021-03-08 22:42:33 +0300

    add invalidation step

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarkettingApp from "./components/MarkettingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there</h1>
+      <h1>Hi there!!</h1>
       <MarkettingApp />
     </div>
   );
