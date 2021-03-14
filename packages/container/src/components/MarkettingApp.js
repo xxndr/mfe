cd0369ad160c44b391e81c044e17ba3e5737c603
ksandr68 <malinsancho@gmail.com>
2021-03-14 12:59:45 +0300

    fds

--- packages/container/src/components/MarkettingApp.js
+++ packages/container/src/components/MarkettingApp.js
@@ -1,13 +1,14 @@
-import { mount } from "marketing/MarketingApp";
+import {mount} from "marketing/MarketingApp";
 
-import React, { useRef, useEffect } from "react";
+import React, {useRef, useEffect} from "react";
 
 export default () => {
   const ref = useRef(null);
+
 
   useEffect(() => {
     mount(ref.current);
   }, []);
 
-  return <div ref={ref} />;
+  return <div ref={ref}/>;
 };
